9264752cba1f320ce241b25ce21eab0c632e61c9
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2023-01-09 17:07:40 -0700

    steps 4 and 5 completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import data from './data';
 
 // Components
+import { ProductContext } from './contexts/ProductContext';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
